a4b5b8a97058dee983a65eb1b1e194def7157de6
kounkourene <kounkourene@gmail.com>
2024-07-03 10:27:51 -0700

    Added Switching the Active player

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 //Selecting elements
+const player0EL = document.querySelector(".player--0");
+const player1EL = document.querySelector(".player--1");
 const score0EL = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
 const currentScore0El = document.getElementById("current--0");
@@ -10,10 +12,12 @@ const rollDice = document.querySelector(".btn--roll");
 const holdDice = document.querySelector(".btn--hold");
 
 //Starting conditions
+const scores = [0, 0];
 score0EL.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 let currentScore = 0;
+let activePlayer = 0;
 
 rollDice.addEventListener("click", function () {
   //Generating a random dice
@@ -23,7 +27,14 @@ rollDice.addEventListener("click", function () {
 
   if (randomDice !== 1) {
     currentScore += randomDice;
-    currentScore0El.textContent = currentScore;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0EL.classList.toggle("player--active");
+    player1EL.classList.toggle("player--active");
   }
 });
